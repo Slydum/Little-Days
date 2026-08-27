fa71d58657890a4b715d14f301098efabc89abb4
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-28 00:43:05 +0800

    Use normalized psychology event classifier

--- game/engine-v29.js
+++ game/engine-v29.js
@@ -1,7 +1,7 @@
-import * as core from "./engine-v28.js?core=psychology-events-v1";
-import { decorateEventWithPsychology } from "./psychology-events.js?v=1";
+import * as core from "./engine-v28.js?core=psychology-events-v2";
+import { decorateEventWithPsychology } from "./psychology-events-v2.js?v=1";
 
-export * from "./engine-v28.js?core=psychology-events-v1";
+export * from "./engine-v28.js?core=psychology-events-v2";
 
 export function getCurrentEvent(state) {
   const event = core.getCurrentEvent(state);
