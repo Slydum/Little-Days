47d64834dc88662af46e9dd83f3a5bd1bc27a39c
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-28 00:43:11 +0800

    Use normalized psychology event classifier

--- game/contextual-events-v2.js
+++ game/contextual-events-v2.js
@@ -1,7 +1,7 @@
-import * as core from "./contextual-events.js?core=psychology-events-v1";
-import { decorateEventWithPsychology } from "./psychology-events.js?v=1";
+import * as core from "./contextual-events.js?core=psychology-events-v2";
+import { decorateEventWithPsychology } from "./psychology-events-v2.js?v=1";
 
-export * from "./contextual-events.js?core=psychology-events-v1";
+export * from "./contextual-events.js?core=psychology-events-v2";
 
 export function contextualEventForState(state) {
   const event = core.contextualEventForState(state);
