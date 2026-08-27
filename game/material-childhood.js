981f75c622c786fe4ec8c44c8d00b6814dddca71
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-28 07:53:18 +0800

    Expose material childhood simulation

---
+++ game/material-childhood.js
@@ -0,0 +1,3 @@
+export { ensureMaterialChildhood, materialSnapshot } from "./material-core.js?v=1";
+export { advanceMaterialChildhood } from "./material-world.js?v=1";
+export { commitMaterialEvent, materialEventForState } from "./material-events.js?v=1";
