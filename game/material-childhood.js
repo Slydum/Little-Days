57ae0e694f0e9fffb5d749a8de06eec0edb74904
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-28 07:55:19 +0800

    Use material childhood compatibility fixes

--- game/material-childhood.js
+++ game/material-childhood.js
@@ -1,3 +1,3 @@
 export { ensureMaterialChildhood, materialSnapshot } from "./material-core.js?v=1";
-export { advanceMaterialChildhood } from "./material-world.js?v=1";
-export { commitMaterialEvent, materialEventForState } from "./material-events.js?v=1";
+export { advanceMaterialChildhood } from "./material-world-v2.js?v=1";
+export { commitMaterialEvent, materialEventForState } from "./material-events-v2.js?v=1";
