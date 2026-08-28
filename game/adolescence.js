45d2f8cb72b57f15b7d92373136800578a628814
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-28 15:37:13 +0800

    Wire teen relationship pressure dynamics into adolescence

--- game/adolescence.js
+++ game/adolescence.js
@@ -1,2 +1,2 @@
-export { adolescenceStage, adolescenceSnapshot, advanceAdolescence, ensureAdolescenceState, queueAdolescenceEvent, teenGrade } from "./adolescence-pregnancy-core.js?v=1";
-export { adolescenceEventForState, applyAdolescenceEffect, commitAdolescenceEvent } from "./adolescence-pregnancy-events-v2.js?v=1";
+export { adolescenceStage, adolescenceSnapshot, advanceAdolescence, ensureAdolescenceState, queueAdolescenceEvent, teenGrade } from "./adolescence-pressure-core.js?v=1";
+export { adolescenceEventForState, applyAdolescenceEffect, commitAdolescenceEvent } from "./adolescence-pressure-events.js?v=1";
