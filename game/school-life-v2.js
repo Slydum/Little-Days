5fe9c8facb23e5c1340f2f2a39ee37cb2d1ff9d2
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-28 08:20:01 +0800

    Add School Life v2 facade

---
+++ game/school-life-v2.js
@@ -0,0 +1,13 @@
+export {
+  advanceSchoolLifeV2,
+  ensureSchoolLifeV2,
+  sanitizeLegacySchoolForPath,
+  schoolLifeSnapshot,
+  schoolModeIsConventional,
+  switchEducationMode,
+} from "./school-life-v2-core.js?v=1";
+export {
+  adaptLegacySchoolEvent,
+  commitSchoolLifeV2Event,
+  schoolLifeV2EventForState,
+} from "./school-life-v2-events.js?v=1";
