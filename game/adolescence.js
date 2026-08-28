1c5132f038d6a798b6a5713b5cdbf26818d620fa
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-28 11:46:10 +0800

    Wire teen pregnancy into adolescence

--- game/adolescence.js
+++ game/adolescence.js
@@ -1,2 +1,2 @@
-export { adolescenceStage, adolescenceSnapshot, advanceAdolescence, ensureAdolescenceState, queueAdolescenceEvent, teenGrade } from "./adolescence-core-v2.js?v=1";
-export { adolescenceEventForState, applyAdolescenceEffect, commitAdolescenceEvent } from "./adolescence-events-v2.js?v=1";
+export { adolescenceStage, adolescenceSnapshot, advanceAdolescence, ensureAdolescenceState, queueAdolescenceEvent, teenGrade } from "./adolescence-pregnancy-core.js?v=1";
+export { adolescenceEventForState, applyAdolescenceEffect, commitAdolescenceEvent } from "./adolescence-pregnancy-events.js?v=1";
