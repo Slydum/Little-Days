b011e4d6aa4118a262c7f85d13f5fcca8ffe1180
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-28 08:34:25 +0800

    Add adolescence module facade

---
+++ game/adolescence.js
@@ -0,0 +1,2 @@
+export { adolescenceStage, adolescenceSnapshot, advanceAdolescence, ensureAdolescenceState, queueAdolescenceEvent, teenGrade } from "./adolescence-core.js?v=1";
+export { adolescenceEventForState, applyAdolescenceEffect, commitAdolescenceEvent } from "./adolescence-events.js?v=1";
